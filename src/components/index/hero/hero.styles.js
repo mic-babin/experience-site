a492ddddb69ee578f23a3ecb14e76dad4b944f46
Michael Babin <mic.babin@outlook.com>
2023-10-02 15:24:39 -0400

    test remove svh

--- src/components/index/hero/hero.styles.js
+++ src/components/index/hero/hero.styles.js
@@ -8,11 +8,10 @@ export const Section = styled(SectionLayout)`
 
 export const Wrapper = styled.div`
   min-height: calc(100vh);
-  min-height: calc(100svh);
 
   @media only screen and (max-width: 767px) {
     height: calc(100vh);
-    height: calc(100svh);
+    height: calc(100vh);
     padding-bottom: 90vh;
     overflow: hidden;
   }
